2a0d5a44eebc21e16a7b15095bd29e8db109ef87
Baharudin Pratama <baharudinp21@gmail.com>
2026-04-28 21:47:36 +0700

    chore(api): update profile api

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -3,22 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Employee;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     public function show($id)
     {
-        $employee = Employee::from('pegawai as p')
-            ->join('detail_pribadi as d', 'd.nomor_urut_pegawai', '=', 'p.nomor_urut_pegawai')
-            ->where('p.nomor_urut_pegawai', $id)
-            ->select([
-                'p.nomor_urut_pegawai',
-                'p.nama',
-                'd.tempat_lahir',
-                'd.tanggal_lahir'
-            ])
+        $employee = User::select('users.*')
+            ->where('nomor_urut_pegawai', $id)
+            ->with('detailPribadi')
+            ->with('divisi')
+            ->with('jabatan')
             ->first();
 
         if (!$employee) {
